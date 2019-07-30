1ee93864f91453228cc24300035bd7516f8f81bf
vpavel04 <32111680+vpavel04@users.noreply.github.com>
2019-07-30 10:10:30 +0300

    Add files via upload

--- dist/MongoDBQueryBuilder.js
+++ dist/MongoDBQueryBuilder.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const db_repository_1 = require("db-repository");
 const mongodb_1 = require("mongodb");
 class MongoDBQueryBuilder {
     all() {
@@ -13,7 +14,14 @@ class MongoDBQueryBuilder {
         return {
             build: () => {
                 const filter = {};
-                filter['_id'] = new mongodb_1.ObjectID(id);
+                if (id instanceof db_repository_1.DbObjectId) {
+                    const dbId = id;
+                    filter['_id'] = new mongodb_1.ObjectID(dbId.value);
+                }
+                else {
+                    const strId = id;
+                    filter['_id'] = new mongodb_1.ObjectID(strId);
+                }
                 return filter;
             }
         };
@@ -22,7 +30,10 @@ class MongoDBQueryBuilder {
         return {
             build: () => {
                 const filter = {};
-                filter[propName] = propVal;
+                if (propVal instanceof db_repository_1.DbObjectId)
+                    filter[propName] = new mongodb_1.ObjectID(propVal.value);
+                else
+                    filter[propName] = propVal;
                 return filter;
             }
         };
@@ -30,9 +41,26 @@ class MongoDBQueryBuilder {
     byProperties(dict) {
         return {
             build: () => {
+                this.updateIdsToMongo(dict);
                 return dict;
             }
         };
+    }
+    updateIdsToMongo(dict) {
+        if (!dict ||
+            typeof (dict) === 'string' || dict instanceof String ||
+            typeof (dict) === 'number' || dict instanceof Number ||
+            typeof (dict) === 'boolean' || dict instanceof Boolean)
+            return;
+        for (const key in dict) {
+            if (dict.hasOwnProperty(key)) {
+                const val = dict[key];
+                if (val instanceof db_repository_1.DbObjectId) {
+                    dict[key] = new mongodb_1.ObjectID(val.value);
+                }
+                this.updateIdsToMongo(val);
+            }
+        }
     }
 }
 exports.MongoDBQueryBuilder = MongoDBQueryBuilder;
